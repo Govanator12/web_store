5dddcd10cddbb82a815f6c73553e1d0fe86b54e4
Steven Hardy <steverhardy@gmail.com>
2019-05-08 13:05:01 -0400

    Updated database

--- config.py
+++ config.py
@@ -9,4 +9,9 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # set the uri for the database location, this db will be locally stored through a sqlite database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+
+
+
+    # use this when setting up postgres
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:300TwDiH!@localhost:5432/twitter'
